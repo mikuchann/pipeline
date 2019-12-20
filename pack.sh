6b1887c06f17a28777b625cf95bf04530cde8a75
adevjoe <adevjoe@gmail.com>
2019-12-20 14:40:25 +0800

    fix pack.sh

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
-project=$(git branch | echo)
+project=$(git symbolic-ref HEAD | sed 's!refs\/heads\/!!')
 echo "打包项目 $project 中..."
 zip -r $project-pipeline-templates.zip definition
 echo "项目 $project 模板打包完成，文件: $project-pipeline-templates.zip"
